9d3cf7cd29d7a8c1509aa96477e4e4a7c50ec6e6
Ewe Tek Min <ewe.tekmin@outlook.com>
2019-04-28 19:48:47 +0800

    Add all plugins

---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:jest/recommended',
+    'plugin:flowtype/recommended',
+    'plugin:react-native/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'jest',
+    'flowtype',
+    'react-hooks',
+  ],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
