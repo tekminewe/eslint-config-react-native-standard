19b6aef00ce1000e4f6e376a8f94d4e794b1cd15
Ewe Tek Min <ewe.tekmin@outlook.com>
2019-09-14 07:25:20 +0800

    Add back airbnb and add shallow in global

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 module.exports = {
   extends: [
+    'airbnb',
     '@react-native-community',
   ],
   globals: {
     jest: true,
+    shallow: true,
   },
   rules: {
     'prettier/prettier': ['error', { singleQuote: true, trailingComma: 'all' }]
