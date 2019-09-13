5c2349ed59a13533d4aa5035ba66c5ef88344b43
Ewe Tek Min <ewe.tekmin@outlook.com>
2019-09-13 23:33:05 +0800

    Use prettier single quote options

--- index.js
+++ index.js
@@ -7,42 +7,6 @@ module.exports = {
     jest: true,
   },
   rules: {
-    quotes: [
-      'error',
-      'single',
-      { 'avoidEscape': true, 'allowTemplateLiterals': false }
-    ]
-  }
-  // parserOptions: {
-  //   ecmaFeatures: {
-  //     jsx: true,
-  //   },
-  //   ecmaVersion: 2018,
-  //   sourceType: 'module',
-  // },
-  // plugins: [
-  //   'react',
-  //   'jest',
-  //   'flowtype',
-  //   'react-hooks',
-  // ],
-  // rules: {
-  //   'react-hooks/rules-of-hooks': 'error',
-  //   'react-hooks/exhaustive-deps': 'warn',
-  //   'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
-  //   'no-underscore-dangle': ['error', { 'allow': ['__DEV__'] }],
-  // },
-  // settings: {
-  //   'import/resolver': {
-  //     'node': {
-  //       'extensions': [
-  //         '.js',
-  //         '.android.js',
-  //         '.ios.js',
-  //         '.ts',
-  //         '.tsx',
-  //       ]
-  //     },
-  //   },
-  // },
+    'prettier/prettier': ['error', { 'singleQuote': true }]
+  },
 };
